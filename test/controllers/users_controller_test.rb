422c2be4981d4450836718dea8014d6d512e6729
Gabe D <gpad88@gmail.com>
2020-04-22 14:05:46 -0700

    Adds test for users controller show action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "show" do
+    user = users(:one)
+    get("/users/#{user.id}")
+    assert_equal("200", @response.code)
+    assert_equal(JSON.parse(user.to_json), json_response(@response.body)["user"])
+  end
 end
