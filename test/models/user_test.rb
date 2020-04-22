550037d4cde941e6769ec9b171439a6717886e3d
Gabe D <gpad88@gmail.com>
2020-04-22 13:36:50 -0700

    Makes name required

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "creates a user" do
-    user = User.create(password: "fakepassword", email: "foo@example.com")
+    user = User.create(name: "moonchild", password: "fakepassword", email: "moonchild@example.com")
     assert_equal(true, user.persisted?)
   end
 end
