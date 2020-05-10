944377f315789543c55da9f4e46e46c520ef5cf2
Gabe D <gpad88@gmail.com>
2020-05-09 20:47:42 -0700

    Disables protect_from_forgery when request format json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery unless: -> { request.format.json? }
 
   def devise_current_user
     @devise_current_user ||= warden.authenticate(:scope => :user)
