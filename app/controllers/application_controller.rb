e3a78ff40754fb624a9cb3723acf3c53929df120
Gabe D <gpad88@gmail.com>
2020-05-11 11:43:42 -0700

    Removes commented out null user method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,4 @@ class ApplicationController < ActionController::Base
   def devise_current_user
     @devise_current_user ||= warden.authenticate(:scope => :user)
   end
-
-  # def current_user
-  #   devise_current_user || NullUser.new
-  # end
 end
