3ba3428a141cb91f4d1b4d7ee44b4ae8df1438cb
Gabe D <gpad88@gmail.com>
2020-09-05 13:44:22 -0700

    Delete copy pasta

--- app/javascript/packs/reducers/root.js
+++ app/javascript/packs/reducers/root.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
-// import reviewReducer from "./reviewReducer";
 
 const rootReducer = combineReducers({
   current_user: userReducer,
-  // reviews: reviewReducer,
 });
 
 export default rootReducer;
